0dff5e3c5efe89e5875388f84f682e476073cf9a
Dennis Lammers <dennislammers@live.nl>
2023-06-29 15:02:33 +0200

    chore: completed course

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 provider "google" {
-  project = var.project
-  credentials=file(var.credentials_file)
-  region=var.region
-  zone = var.zone
+  project     = var.project
+  credentials = file(var.credentials_file)
+  region      = var.region
+  zone        = var.zone
 }
 
 resource "google_compute_instance" "test" {
-    name="test-instance"
-    machine_type="e2-micro"
-    zone="europe-west4-b"
-    allow_stopping_for_update=true
+  name                      = var.vm_params.name
+  machine_type              = var.vm_params.machine_type
+  zone                      = var.vm_params.zone
+  allow_stopping_for_update = var.vm_params.allow_stopping_for_update
 
-    boot_disk {
-        initialize_params {
-            image="cos-cloud/cos-stable"
-        }
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
     }
+  }
 
-    network_interface {
-        network="default"
-        access_config {
-            //
-        }
+  network_interface {
+    network = "default"
+    access_config {
+      //
     }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,31 @@
 variable "project" {}
 variable "credentials_file" {}
 variable "region" {
-    default="europe-west4"
+  default     = "europe-west4"
+  description = "This is the region everything is deployed for"
+  type        = string
 }
 variable "zone" {
-    default="europe-west4a"
+  default = "europe-west4a"
+  type    = string
 }
 variable "os_image" {
-    default="debian-cloud/debian-9"
+  default = "debian-cloud/debian-9"
+  type    = string
+}
+
+variable "vm_params" {
+  type = object({
+    name                      = string
+    machine_type              = string
+    zone                      = string
+    allow_stopping_for_update = bool
+  })
+  description = "Een beschrijving"
+  default = {
+    name                      = "test-instance"
+    machine_type              = "e2-micro"
+    zone                      = "europe-west4"
+    allow_stopping_for_update = true
+  }
 }
